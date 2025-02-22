0049b95bc10dc99bee0dc8ecedc85435ee75ee97
Carlos Rangel <carlos@rangel.us>
2025-02-22 11:58:00 -0800

    test

--- frontend/src/components/nodes/prompt-node.tsx
+++ frontend/src/components/nodes/prompt-node.tsx
@@ -4,14 +4,16 @@ import { Handle, Node, NodeProps, Position,  } from '@xyflow/react';
 import { useState } from 'react';
 import { Card, CardContent } from '../ui/card';
 import { Input } from '../ui/input';
+import { Button } from '../ui/button';
 export type PromptNodeData = Node<{
   prompt: string;
 }>
 export function PromptNode({ data, isConnectable }: NodeProps<PromptNodeData>) {
     const [prompt, setPrompt] = useState(data.prompt);
   return <Card >
-<CardContent>
+<CardContent className='flex flex-col gap-2 p-2'>
     <Input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} />
+    <Button>Generate</Button>
     </CardContent>
 
     <Handle type="source" position={Position.Top} />
