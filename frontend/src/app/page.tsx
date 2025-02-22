c18eeeee9466e146a8bf39027b191b0ad40115b5
Carlos Rangel <carlos@rangel.us>
2025-02-22 11:52:37 -0800

    yeet

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,34 @@
-"use client";
+"use client"
 
+import { PromptNode } from "@/components/nodes/prompt-node";
 import { ReactFlow, ReactFlowProvider } from "@xyflow/react";
-import TextInput from "./components/text-input";
-import Chat from "./components/chat";
+import '@xyflow/react/dist/style.css';
+import { useMemo } from "react";
+
+const initialNodes = [
+  { id: '1', type: 'prompt', position: { x: 0, y: 0 }, data: { prompt: 'Hello, world!' } },
+];
+
+const initialEdges = [
+  { id: 'e1-2', source: '1', target: '2' },
+];
 
 export default function HomePage() {
+  const nodeTypes = useMemo(() => ({
+    prompt: PromptNode,
+  }), []);
   return (
-    <>
-      <ReactFlowProvider>
-        <Chat></Chat>
-        <TextInput></TextInput>
-
-        {/* <ReactFlow /> */}
-      </ReactFlowProvider>
-    </>
+   <div className="h-screen w-full">
+   <ReactFlowProvider>
+    <ReactFlow
+      nodeTypes={nodeTypes}
+      proOptions={{
+        hideAttribution: true,
+      }}
+      nodes={initialNodes}
+      edges={initialEdges}
+    />
+   </ReactFlowProvider>
+   </div>
   );
 }
