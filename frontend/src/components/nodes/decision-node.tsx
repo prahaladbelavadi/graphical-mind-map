0760f7b6aab5110fbef2658799db9aa797461e68
Nikhil Kulkarni <nikhilkulkarni1755@gmail.com>
2025-02-22 12:56:21 -0800

    decision node init

---
+++ frontend/src/components/nodes/decision-node.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Handle, Node, NodeProps, Position } from "@xyflow/react";
+import { useEffect, useState } from "react";
+import { Card, CardContent } from "../ui/card";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { experimental_useObject as useObject } from "@ai-sdk/react";
+import { nodeSchema } from "@/types/schema";
+import { generateId } from "ai";
+export type PromptNodeData = Node<{
+  prompt: string;
+}>;
+
+export function DecisionNode({
+  id,
+  data,
+  isConnectable,
+}: NodeProps<PromptNodeData>) {
+  const [prompt, setPrompt] = useState(data.prompt);
+  const { submit, isLoading, error, object } = useObject({
+    id,
+    api: "/api/ai",
+    schema: nodeSchema,
+    onFinish: ({ object }) => {
+      console.log(object);
+      if (object?.nodes.length) {
+        object.nodes.forEach((node) => {
+          const newNodeId = generateId();
+          const newNode = {
+            id: newNodeId,
+            type: node.type,
+            data: node.data,
+            position: { x: 0, y: 0 },
+          };
+          const newEdge = {
+            id: `${id}-${newNodeId}`,
+            source: id,
+            target: newNodeId,
+          };
+        });
+      }
+    },
+  });
+  // Streamed object
+  // useEffect(() => {
+  //   if (object) {
+  //     console.log(object);
+  //   }
+  // }, [object]);
+  const handleSubmit = () => {
+    submit({ prompt });
+  };
+  return (
+    <Card>
+      <CardContent className="flex flex-col gap-2 p-2">
+        <Input
+          type="text"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+        <Button onClick={handleSubmit}>Generate</Button>
+      </CardContent>
+      <Handle type="source" position={Position.Top} />
+      <Handle type="target" position={Position.Bottom} />
+    </Card>
+  );
+}
